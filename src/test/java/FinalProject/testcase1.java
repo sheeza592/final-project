41248e4d8ad4d0c638289671a4d5f33fb657e291
sheeza592 <sheeza_ashraf@hotmail.com>
2024-07-29 09:47:43 -0700

    first commit

---
+++ src/test/java/FinalProject/testcase1.java
@@ -0,0 +1,64 @@
+package FinalProject;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+
+
+
+
+public class testcase1 {
+
+	
+	
+	@Test
+	public void adduser() throws InterruptedException 
+	{
+		
+		WebDriver driver=new FirefoxDriver();
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+
+		Thread.sleep(2000);
+		
+		driver.findElement(By.xpath("/html/body/div/div[1]/div/div[1]/div/div[2]/div[2]/form/div[1]/div/div[2]/input")).sendKeys("Admin");
+		driver.findElement(By.xpath("/html/body/div/div[1]/div/div[1]/div/div[2]/div[2]/form/div[2]/div/div[2]/input")).sendKeys("admin123");
+		driver.findElement(By.xpath("/html/body/div/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button")).click();
+		Thread.sleep(2000);
+        driver.findElement(By.xpath("/html/body/div/div[1]/div[1]/aside/nav/div[2]/ul/li[1]/a/span")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("/html/body/div/div[1]/div[1]/header/div[2]/nav/ul/li[1]/span")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("/html/body/div/div[1]/div[1]/header/div[2]/nav/ul/li[1]/ul/li/a")).click();
+	    Thread.sleep(2000);
+	    driver.findElement(By.xpath("/html/body/div/div[1]/div[2]/div[2]/div/div[2]/div[1]/button")).click();
+	    Thread.sleep(2000);
+	   driver.findElement(By.xpath("/html/body/div/div[1]/div[2]/div[2]/div/div/form/div[1]/div/div[1]/div/div[2]/div/div/div[1]")).click();
+	    Thread.sleep(2000);
+	    driver.findElement(By.xpath("/html/body/div/div[1]/div[2]/div[2]/div/div/form/div[1]/div/div[1]/div/div[2]/div/div[2]/div[3]")).click();
+	    
+	    driver.findElement(By.xpath("/html/body/div/div[1]/div[2]/div[2]/div/div/form/div[1]/div/div[2]/div/div[2]/div/div/input")).sendKeys("C");
+	    Thread.sleep(2000);
+		driver.findElement(By.xpath("//label[text()=\"Employee Name\"]/../..//div[@role='option']")).click();
+	    Thread.sleep(2000);
+	    driver.findElement(By.xpath("/html/body/div/div[1]/div[2]/div[2]/div/div/form/div[1]/div/div[4]/div/div[2]/input")).sendKeys("Charlie");
+	    Thread.sleep(2000);
+	    driver.findElement(By.xpath("/html/body/div/div[1]/div[2]/div[2]/div/div/form/div[1]/div/div[3]/div/div[2]/div/div/div[1]")).click();
+	    Thread.sleep(2000);
+	    driver.findElement(By.xpath("/html/body/div/div[1]/div[2]/div[2]/div/div/form/div[1]/div/div[3]/div/div[2]/div/div[2]/div[2]")).click();
+	
+	    Thread.sleep(2000);
+	    driver.findElement(By.xpath("/html/body/div/div[1]/div[2]/div[2]/div/div/form/div[2]/div/div[1]/div/div[2]/input")).sendKeys("john123");
+	
+	    Thread.sleep(2000);
+	    driver.findElement(By.xpath("/html/body/div/div[1]/div[2]/div[2]/div/div/form/div[2]/div/div[2]/div/div[2]/input")).sendKeys("john123");
+	
+	    Thread.sleep(2000);
+	    driver.findElement(By.xpath("/html/body/div/div[1]/div[2]/div[2]/div/div/form/div[3]/button[2]")).click();
+	
+	
+	}
+}
---
+++ src/test/java/FinalProject/testcase3.java
@@ -0,0 +1,32 @@
+package FinalProject;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class testcase3 {
+	
+	@Test
+	public void adduser() throws InterruptedException 
+	{
+		
+		WebDriver driver=new FirefoxDriver();
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("/html/body/div/div[1]/div/div[1]/div/div[2]/div[2]/form/div[1]/div/div[2]/input")).sendKeys("Admin");
+		driver.findElement(By.xpath("/html/body/div/div[1]/div/div[1]/div/div[2]/div[2]/form/div[2]/div/div[2]/input")).sendKeys("admin123");
+		driver.findElement(By.xpath("/html/body/div/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button")).click();
+		Thread.sleep(2000);
+		
+		driver.findElement(By.xpath("/html/body/div/div[1]/div[1]/aside/nav/div[2]/ul/li[1]/a/span")).click();
+        Thread.sleep(2000);
+        
+        driver.findElement(By.xpath("/html/body/div/div[1]/div[2]/div[2]/div/div[1]/div[2]/form/div[1]/div/div[1]/div/div[2]/input")).sendKeys("Charlie");	
+        
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("/html/body/div/div[1]/div[2]/div[2]/div/div[1]/div[2]/form/div[2]/button[2]")).click();
+
+}
+}
